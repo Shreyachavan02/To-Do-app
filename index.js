a2b2d4ab0647fbf833b19144741af07334ea01c6
Shreyachavan02 <chavanshreya654@gmail.com>
2025-10-27 22:13:59 +0530

    get method completed

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const TODO_ITEMS = [
   { id: 2, task: 'go to college' },
   { id: 3, task: 'Read a book' },
 ];
- app.get('/todos', (req, res) => {
-   res.json(TODO_ITEMS);
- }
+
+app.get('/todos', (req, res) => {
+  res.json({
+    success: true,
+    data: TODO_ITEMS,
+    message: 'To-Do items retrieved successfully',
+  });
+});
 
 app.listen(8080, () => {
-  console.log('Server is running on port 3000');
+  console.log('✅ Server is running on port 8080');
 });
