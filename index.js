cafd362a00de7c2cc6889b509da767e7297813d9
Shreyachavan02 <chavanshreya654@gmail.com>
2025-10-28 16:55:20 +0530

    env file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -178,6 +179,8 @@ res.json({
 });
 
 });
-app.listen(8080, () => {
-  console.log('✅ Server is running on port 8080');
+
+const PORT = process.env.PORT || 8080;
+app.listen( PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
