23c0a992200d6355713517627caa43c206026b8a
Shreyachavan02 <chavanshreya654@gmail.com>
2025-10-28 13:31:48 +0530

    path parameter pass

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+
+
 const TODO_ITEMS = [
-  { id: 1, task: 'finish icp assignment' },
-  { id: 2, task: 'go to college' },
-  { id: 3, task: 'Read a book' },
+  {
+    id: 1,
+    task: 'Buy groceries',
+    priority: 'High',
+    emoji: '🛒',
+    isdone: false,
+    createdAt: '2024-06-01T10:00:00Z',
+  },
+  {
+    id: 2,
+    task: 'Attend guest',
+    priority: 'high',
+    emoji: '🏠',
+    isdone: false,
+    createdAt: '2024-06-02T12:00:00Z',
+  },
+  {
+    id: 3,
+    task: 'Finish project report',
+    priority: 'Medium',
+    emoji: '📄',
+    isdone: false,
+    createdAt: '2024-06-03T15:30:00Z',
+  },
 ];
 
 app.get('/todos', (req, res) => {
@@ -15,6 +40,50 @@ app.get('/todos', (req, res) => {
     message: 'To-Do items retrieved successfully',
   });
 });
+app.post("/todos", (req, res) => {
+  const { todoItem, priority, emoji } = req.body;
+
+const todoObj= {
+  id: TODO_ITEMS[TODO_ITEMS.length -1].id +1,
+  task: todoItem,
+  priority: priority,
+  emoji: emoji,
+  isdone: false,
+  createdAt: new Date().toISOString(),
+};
+
+  TODO_ITEMS.push(todoObj);
+
+  res.json({
+    success: true,
+    data: todoObj,
+    message: "Todo item added successfully"
+  });
+});
+
+app.get("/todos/:id", (req, res) => {
+  
+const { id } = req.params; 
+
+const todoItem = TODO_ITEMS.find((item) => {
+ if(item.id == id) return item;
+});
+if(todoItem){
+  res.json({
+    success: true,
+    data: todoItem,
+    message: "Todo item fetched successfully",
+  });
+}
+else {
+  res.json({
+    success: false,
+    data: null, 
+    message: "Todo item not found",
+});
+}
+});
+
 
 app.listen(8080, () => {
   console.log('✅ Server is running on port 8080');
